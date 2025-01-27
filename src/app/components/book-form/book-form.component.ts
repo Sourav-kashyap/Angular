7b64b330bb42243be95ec6d3ab9b3ba2c5f816c7
Sourav-kashyap <sourav.kashyap@sourcefuse.com>
2025-01-27 18:13:35 +0530

    Add form validations for book details form

--- src/app/components/book-form/book-form.component.ts
+++ src/app/components/book-form/book-form.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-book-form',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, ReactiveFormsModule,NgFor],
   templateUrl: './book-form.component.html',
-  styleUrl: './book-form.component.css'
+  styleUrl: './book-form.component.css',
 })
 export class BookFormComponent {
+  constructor(private router: Router) {}
+  categories = [
+    'fiction',
+    'non-fiction',
+    'fantasy',
+    'science',
+    'biography',
+    'history',
+    'horror',
+    'children',
+    'other',
+  ];
 
+  bookDetails: FormGroup = new FormGroup({
+    title: new FormControl('', [Validators.required]),
+    author: new FormControl('', [Validators.required]),
+    isbn: new FormControl('', [Validators.required,Validators.minLength(4)]),
+    publishDate: new FormControl('', [Validators.required]),
+    category: new FormControl('', [Validators.required]),
+    price: new FormControl('', [Validators.required, Validators.min(1)]),
+  });
+
+  onSubmit() {
+    console.log(this.bookDetails.value);
+    
+    this.router.navigate(['/display-books']);
+  }
 }
