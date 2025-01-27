ff9229d398c941dafe95aa7f3fa94dd8bf3df0b2
Sourav-kashyap <sourav.kashyap@sourcefuse.com>
2025-01-27 16:38:03 +0530

    Add DisplayBook component

--- src/app/components/display-book/display-book.component.ts
+++ src/app/components/display-book/display-book.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-display-book',
   imports: [NavbarComponent],
   templateUrl: './display-book.component.html',
-  styleUrl: './display-book.component.css'
+  styleUrl: './display-book.component.css',
 })
 export class DisplayBookComponent {
-
+  constructor(private router: Router) {}
+  navigateToBookForm() {
+    this.router.navigate(['/book-form']); // Navigate to the book form page
+  }
 }
