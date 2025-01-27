a122d5a7b13b4a93e2bc893e8a7a1cd3999c4ed1
Sourav-kashyap <sourav.kashyap@sourcefuse.com>
2025-01-27 15:57:40 +0530

    Setup tailwindCss, Bootstrap, Routing and add some basic component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { BookFormComponent } from './components/book-form/book-form.component';
+import { DisplayBookComponent } from './components/display-book/display-book.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'' , redirectTo:'home',pathMatch:'full'},
+    {
+        path:'home',component:HomeComponent
+    },
+    {
+        path:'book-form',component:BookFormComponent
+    },
+    {
+        path:'display-book',component:DisplayBookComponent
+    }
+];
---
+++ src/app/components/display-book/display-book.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-display-book',
+  imports: [],
+  templateUrl: './display-book.component.html',
+  styleUrl: './display-book.component.css'
+})
+export class DisplayBookComponent {
+
+}
